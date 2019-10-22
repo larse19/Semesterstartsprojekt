79d89df5a490257eb626ee506035adc40d51117a
Vincent <Vincent@DESKTOP-GOP1CI9>
2019-10-22 14:31:01 +0200

    inventory og item class

--- code/src/main/java/worldOfZuul/Inventory.java
+++ code/src/main/java/worldOfZuul/Inventory.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package worldOfZuul;
 
-/**
- *
- * @author Vincent
- */
+import java.util.HashMap;
+
 public class Inventory {
+    private HashMap<String,Integer> itemList = new HashMap<String,Integer>();
+    
+    public void putItem(String itemName, int itemCount){
+        boolean exist = false;
+        for(String i : itemList.keySet()){
+            if(i == itemName){
+                itemList.put(i, itemList.get(i)+itemCount);
+                exist = true;
+            }
+        }
+        if (!exist){
+            itemList.put(itemName, itemCount);
+        }
+    }
+    
+    public boolean removeItem(String itemName, int itemCount){
+        if (itemCount < itemList.get(itemName)){
+            itemList.put(itemName, itemList.get(itemName)-itemCount);
+            return true;
+        } else if(itemCount == itemList.get(itemName)) {
+            itemList.remove(itemName);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public void getItem(String itemName){
+        itemList.get(itemName);
+    }
     
 }
