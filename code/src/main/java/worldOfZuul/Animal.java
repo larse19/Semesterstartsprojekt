4e7bbc4341ce4df67ec2efdaee7b5324bef785bd
Anders Larsen <>
2019-10-23 14:45:22 +0200

    Updated to handle inventory space

--- code/src/main/java/worldOfZuul/Animal.java
+++ code/src/main/java/worldOfZuul/Animal.java
@@ -1,14 +1,14 @@
 package worldOfZuul;
 
 public class Animal {
+
     private String name;
     private Item product;
     private boolean fed;
     private String animalFood;
-    private int foodAmount;
-    private int yieldCount;
-    
-    public Animal(String name, Item product){
+    private int foodAmount, yieldCount;
+
+    public Animal(String name, Item product) {
         this.name = name;
         this.product = product;
         this.animalFood = "Seeds";
@@ -16,8 +16,8 @@ public class Animal {
         this.foodAmount = 1;
         this.yieldCount = 1;
     }
-    
-    public Animal(String name, Item product, String animalFood, int foodAmount, int yieldCount){
+
+    public Animal(String name, Item product, String animalFood, int foodAmount, int yieldCount) {
         this.name = name;
         this.product = product;
         this.animalFood = animalFood;
@@ -25,34 +25,45 @@ public class Animal {
         this.foodAmount = foodAmount;
         this.yieldCount = yieldCount;
     }
-    
+
     //Feeds the animal
-    public void feed(Inventory inventory){
-        this.fed = true;
-        inventory.removeItem(this.animalFood, this.foodAmount);
-        System.out.println("You have fed the " + this.name);
-    }        
-    
-    public boolean getFed(){
+    public void feed(Inventory inventory) {
+        if (!fed) {
+            if (inventory.removeItem(this.animalFood, this.foodAmount)) {
+
+                this.fed = true;
+                System.out.println("You have fed the " + this.name + ".");
+            } else {
+                System.out.println("You don't have any " + this.animalFood + " to feed the " + this.name + ".");
+            }
+        } else {
+            System.out.println(this.name + " is already fed.");
+        }
+    }
+
+    public boolean getFed() {
         return this.fed;
     }
-    
-    public void setFed(boolean fed){
+
+    public void setFed(boolean fed) {
         this.fed = fed;
     }
-    
+
     //Collects product from animal, and adds it to inventory, if the animal is fed
-    public void collectProduct(Inventory inventory){
-        if(this.fed){
-            this.fed = false;
+    public void collectProduct(Inventory inventory) {
+        if (this.fed) {
             //Missing ingredient class to finish; following is example
             Ingredient product = new Ingredient(this.product);
-            inventory.putItem(product, this.yieldCount);
-            System.out.println("You have collected " + this.yieldCount + this.product);
-        }
-        else{
-            System.out.println(this.name + " needs to be fed");
+            if (inventory.putItem(product, this.yieldCount)) {
+                this.fed = false;
+                System.out.println("You have collected " + this.yieldCount + this.product + ".");
+            }
+            else{
+                System.out.println("You don't have enough room in your inventory.");
+            }
+        } else {
+            System.out.println(this.name + " needs to be fed.");
         }
     }
-    
+
 }
