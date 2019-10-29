064fdc68014489693e60cc3a2215a507d2a6efa0
Vincent Bjørneset <vinbjo152@gmail.com>
2019-10-29 12:44:01 +0100

    item update

--- code/src/main/java/worldOfZuul/Item.java
+++ code/src/main/java/worldOfZuul/Item.java
@@ -1,31 +1,37 @@
 package worldOfZuul;
 
 public class Item {
-    private String name;
-    private int saturation;
+    protected String name;
+    protected int saturation;
     
+    //constructor for item with 0 saturation
     public Item(String name){
         this.name = name;
         this.saturation = 0;
     }
     
+    //constructor for item with set saturation
     public Item(String name, int sat){
         this.name = name;
         saturation = sat;
     }
     
+    //getter for item name
     public String getName(){
         return name;
     }
     
+    //setter for item name
     public void setName(String itemName){
         name = itemName;
     }
     
+    //getter for item saturation
     public int getSaturation(){
         return saturation;
     }
     
+    //setter for saturation
     public void setSaturation(int itemSaturation){
         saturation = itemSaturation;
     }
