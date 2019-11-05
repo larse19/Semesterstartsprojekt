e977629182d8771ce7f31aac95bfbceceb6b24ba
Vincent <Vincent@DESKTOP-GOP1CI9>
2019-11-05 09:13:32 +0100

    Fiksede food constructor bug

--- code/src/main/java/worldOfZuul/Food.java
+++ code/src/main/java/worldOfZuul/Food.java
@@ -1,25 +1,10 @@
 package worldOfZuul;
 
 public class Food extends Item{
-    private Recipe recipe;
     
     //food constructorr
-    public Food(String name, int saturation, Recipe recipe){
+    public Food(String name, int saturation){
         super(name, saturation);
-        this.recipe = recipe;
-    }
-    
-    //getter for recipe
-    public Recipe getRecipe(){
-        return recipe;
-    }
-    
-    /*
-    setter for recipe that food uses. This can be used to check 
-    if food object contains recipe object ect.
-    */
-    public void setRecipe(Recipe foodRecipe){
-        recipe = foodRecipe;
     }
     
 }
