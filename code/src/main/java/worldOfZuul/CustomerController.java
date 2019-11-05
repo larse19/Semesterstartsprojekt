0ae5100df516af04b37540d4f065ef8e855c8ce2
larse19 <anders.l.h.larsen@live.dk>
2019-11-05 10:48:07 +0100

    CustomerController now removes items from inv

--- code/src/main/java/worldOfZuul/CustomerController.java
+++ code/src/main/java/worldOfZuul/CustomerController.java
@@ -1,28 +1,47 @@
 package worldOfZuul;
 
-
-public class CustomerController extends Room{
+public class CustomerController extends Room {
 
     private Customer currentCustomer;
     private Scoreboard sb;
-    
-    
-    public CustomerController(String description){
+    private Inventory inv;
+
+    public CustomerController(String description) {
         super(description);
         currentCustomer = new Customer();
-        
+
     }
-    
-    public void feedCustomer(Food food){
-        
-        int sat = food.getSaturation();
-        
-        currentCustomer.addHP(sat);
+
+    public void feedCustomer(Food food) {
+
+        if (inv.removeItem(food.getName(), 1)) {
+            int sat = food.getSaturation();
+
+            currentCustomer.addHP(sat);
+        } else {
+            System.out.println("You don't have any " + food.getName());
+        }
     }
-    
-    public void newCustomer(){
+
+    public void feedCustomer(Ingredient ingredient) {
+
+        if (ingredient.getEdible()) {
+            if (this.inv.removeItem(ingredient.getName(), 1)) {
+                int sat = ingredient.getSaturation();
+                currentCustomer.addHP(sat);
+            } else {
+                System.out.println("You don't have any " + ingredient.getName());
+            }
+
+        }
+        else{
+            System.out.println("You can't eat " + ingredient.getName());
+        }
+    }
+
+    public void newCustomer() {
         sb.addPoints(currentCustomer.getGainedScore());
         currentCustomer = new Customer();
     }
-    
+
 }
